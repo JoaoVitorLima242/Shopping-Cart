d21e804c97740d261d8a78b932643f955949aa87
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-07 02:41:24 -0300

    import components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components
+import { 
+  Drawer,
+  LinearProgress,
+  Grid,
+  Badge
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
 }
